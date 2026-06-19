20e07c0fa407f599ad18ee3623ae2047aff3c588
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-19 04:08:52 +0330

    v1.6

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
+import Footer from "./Components/Footer";
 import Todos from "./Components/Todos";
 import TodoStatus from "./Components/TodoStatus";
 
-
-
 const App = () => {
   return (
-    <div className="flex flex-col justify-center item-center gap-3 bg-white pt-8 pl-4 pr-4">
+    <div className="flex flex-col justify-center item-center gap-3 bg-white pt-8 pl-4 pr-4 text-center">
       <TodoStatus />
       <Todos />
+      <Footer />
     </div>
   );
 };
---
+++ src/Components/Footer.jsx
@@ -0,0 +1,22 @@
+const Footer = () => {
+  return (
+    <div>
+      <>
+        <div
+          className="pt-12
+        "
+        >
+          Made with ❤️ by
+          <a
+            className="text-[#6e757c] text-2xl font-bold"
+            href="https://github.com/hamedmoyasseri2012-tech"
+          >
+            Hamed Moyasseri
+          </a>
+        </div>
+      </>
+    </div>
+  );
+};
+
+export default Footer;
