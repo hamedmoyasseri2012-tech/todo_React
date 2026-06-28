adfbdf072f5a728d3a9f5a8727b9e1f3f093c4d2
milan1990 <hamed.moyasseri2012@gmail.com>
2026-06-28 23:50:25 +0330

    v1.9

--- src/Components/Todos.jsx
+++ src/Components/Todos.jsx
@@ -8,10 +8,6 @@ import { CiEdit } from "react-icons/ci";
 import { MdOutlineDone } from "react-icons/md";
 import { RiDeleteBin6Line } from "react-icons/ri";
 
-
-
-
-
 const Todos = () => {
   const [title, setTitle] = useState("");
   const [data, setData] = useState("");
@@ -20,9 +16,26 @@ const Todos = () => {
   const addTodo = (newItem) => {
     setTodo((prev) => [...prev, { ...newItem, status: "pending" }]);
   };
-  // const deleteTodo = () => {
-  //   setTodo(())
-  // }
+  const deleteTodo = (id) => {
+    setTodo((prev) => prev.filter((item) => item.id !== id));
+  };
+
+  const handleDeleteAll = () => {
+    setTodo([]);
+  };
+
+  const handleDone = (id) => {
+    setTodo((prev) =>
+      prev.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              status: item.status === "pending" ? "completed" : "pending",
+            }
+          : item,
+      ),
+    );
+  };
 
   return (
     <div className="flex flex-col gap-4 w-full">
@@ -78,7 +91,10 @@ const Todos = () => {
       </form>
 
       <div className="flex justify-end">
-        <button className="bg-[#f97172] flex gap-2 items-center pl-2 rounded-xl w-30 h-12">
+        <button
+          onClick={handleDeleteAll}
+          className="bg-[#f97172] flex gap-2 items-center pl-2 rounded-xl w-30 h-12"
+        >
           <i className="text-black">
             <RiDeleteBin5Line />
           </i>
@@ -102,7 +118,7 @@ const Todos = () => {
                 {item.title}
               </td>
               <td className="w-48.75 h-14 items-center flex">{item.data}</td>
-              <td className="border rounded-xl p-4 w-15.5 items-center flex text-xs text-center justify-center border-amber-400 h-5 bg-amber-400">
+              <td className="border rounded-xl p-4 w-15.5 items-center flex text-xs text-center justify-center border-amber-400 h-5 bg-amber-400 ">
                 <p>{item.status}</p>
               </td>
               <td className="gap-3 flex items-center ">
@@ -111,12 +127,18 @@ const Todos = () => {
                     <CiEdit />
                   </i>
                 </button>
-                <button className="w-10 h-6 border flex justify-center text-center items-center rounded-xl text-black bg-[#3ad097]">
+                <button
+                  onClick={() => handleDone(item.id)}
+                  className="w-10 h-6 border flex justify-center text-center items-center rounded-xl text-black bg-[#3ad097]"
+                >
                   <i>
                     <MdOutlineDone />
                   </i>
                 </button>
-                <button  className="w-10 h-6 border flex justify-center text-center items-center rounded-xl text-black bg-[#f77072]">
+                <button
+                  onClick={() => deleteTodo(item.id)}
+                  className="w-10 h-6 border flex justify-center text-center items-center rounded-xl text-black bg-[#f77072]"
+                >
                   <i>
                     <RiDeleteBin6Line />
                   </i>
